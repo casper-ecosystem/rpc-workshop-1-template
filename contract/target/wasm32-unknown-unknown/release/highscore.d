60f8a865b6cacc958b1e73121a8ef894c9db2630
Dylan Ireland <dylan@casper.network>
2022-03-23 08:54:55 -0400

    Replaced full contract with empty contract

--- contract/target/wasm32-unknown-unknown/release/highscore.d
+++
@@ -1,1 +0,0 @@
-/Users/dylanireland/Desktop/CASPER/rpc-workshop-1/contract/target/wasm32-unknown-unknown/release/highscore.wasm: /Users/dylanireland/Desktop/CASPER/rpc-workshop-1/contract/contract/src/highscore.rs
